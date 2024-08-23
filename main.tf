f92ac8bf17764e2064521db2ae6b1dc4278a9b6e
김 영창 <yckim@daolcompany.com>
2024-08-23 11:10:38 +0900

    cicd update

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ module "eks" {
   source = "./modules/eks-cluster"
   cluster_name = "fast-cluster"
   cluster_version = "1.29"
-  vpc_id = "vpc-07e3e196e806c5972"
+  vpc_id = "vpc-0f48398d7ef5a8c6d"
 
-  private_subnets = ["subnet-04c853b7e929e8270", "subnet-0c04f95564c07a34f"]
-  public_subnets  = ["subnet-0be886d2b66cb612e", "subnet-0be30742607e390a8"]
+  private_subnets = ["subnet-0a2e3b135eba3e404", "subnet-03012a71ae9f649c0"]
+  public_subnets  = ["subnet-08fa7f01ce73e4f53", "subnet-03888f7e7928f2176"]
 }
